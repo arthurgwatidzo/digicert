b9c422b42d61651c43cbfa0120e47df1b02418e5
Arthur Gwatidzo <arthurgwatidzo@users.noreply.github.com>
2023-11-29 18:01:31 +0200

    Update ReservationEntityMapper.java

--- src/main/java/com/digicert/hotel/booking/mapper/ReservationEntityMapper.java
+++ src/main/java/com/digicert/hotel/booking/mapper/ReservationEntityMapper.java
@@ -6,10 +6,18 @@ import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ReservationEntityMapper {
+
     ReservationEntity toEntity(ReservationDTO reservationDTO);
 
     ReservationDTO toDto(ReservationEntity reservationEntity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     ReservationEntity partialUpdate(ReservationDTO reservationDTO, @MappingTarget ReservationEntity reservationEntity);
+
+    default void updateExistingEntity(ReservationDTO reservationDTO, ReservationEntity reservationEntity) {
+        // Map non-null fields from DTO to Entity
+        partialUpdate(reservationDTO, reservationEntity);
+    }
+
+
 }
